b684f39e1b9fe6f544b7fc850f369c6a06270d18
jarco0204 <jaarcos@mun.ca>
2025-03-18 21:49:22 -0230

    add: shopping cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/product/product.module';
+import { ShoppingCartModule } from './modules/shopping-cart/shopping-cart.module';
+import { ShoppingCartService } from './modules/shopping-cart/shopping-cart.service';
+import { ShoppingCartController } from './modules/shopping-cart/shopping-cart.controller';
 
 @Module({
-  imports: [ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductModule, ShoppingCartModule],
+  controllers: [AppController, ShoppingCartController],
+  providers: [AppService, ShoppingCartService],
 })
 export class AppModule {}
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from 'src/models/Product';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  // Class property to store products
+  private products = [
+    {
+      id: '1',
+      name: 'Product 1',
+      description: 'Description of product 1',
+      price: 100,
+      stock: 10,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      description: 'Description of product 2',
+      price: 200,
+      stock: 20,
+    },
+  ];
+
+  // Getter for products
+  getProducts() {
+    return this.products;
+  }
+
+  // Overloaded getter for products using id
+  getProduct(id: string) {
+    return this.products.find((product) => product.id === id);
+  }
+
+  // TODO: Implement Create Product
+  createProduct() {}
+
+  // Method to replace product object in array
+  updateProduct(id: string, product: Product) {
+    const index = this.products.findIndex((product) => product.id === id);
+    this.products[index] = product;
+    return product;
+  }
+
+  // Method to delete product object from array
+  deleteProduct(id: string) {
+    const index = this.products.findIndex((product) => product.id === id);
+    this.products.splice(index, 1);
+    return id;
+  }
+}
