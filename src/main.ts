71ed2a7de3bfe11266b751dfd4fcaee07a4c94b0
jarco0204 <jaarcos@mun.ca>
2025-03-18 21:33:04 -0230

    add: product module

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+// TODO: Add general app-error handling
+bootstrap().catch((err) => console.error(err));
