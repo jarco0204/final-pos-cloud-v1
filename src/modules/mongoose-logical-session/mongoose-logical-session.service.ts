28d1043165a2a54678a5601f2c120c9a5c6c9519
jarco0204 <jaarcos@mun.ca>
2025-03-21 09:50:53 -0230

    add: method logical session

--- src/modules/mongoose-logical-session/mongoose-logical-session.service.ts
+++ src/modules/mongoose-logical-session/mongoose-logical-session.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  LogicalSession,
+  LogicalSessionDocument,
+} from 'src/schemas/MongooseLogicalSession';
 
 @Injectable()
-export class MongooseLogicalSessionService {}
+export class MongooseLogicalSessionService {
+  constructor(
+    @InjectModel(LogicalSession.name)
+    private logicalSessionModel: Model<LogicalSessionDocument>,
+  ) {}
+
+  async createLogicalSession(timeout: number): Promise<LogicalSessionDocument> {
+    const session = new this.logicalSessionModel({
+      status: 'pending',
+      timeout,
+    });
+    return session.save();
+  }
+
+  async updateLogicalSessionStatus(
+    logicalSessionId: string,
+    status: 'committed' | 'rolled_back',
+  ): Promise<LogicalSessionDocument | null> {
+    return this.logicalSessionModel
+      .findByIdAndUpdate(logicalSessionId, { status }, { new: true })
+      .exec();
+  }
+}
